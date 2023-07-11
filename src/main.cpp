4a194a505ef7395ae1d0b288e1809baa9cf165ea
janith <janithravi7@gmail.com>
2023-07-11 13:10:00 +0530

    pulse

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,48 @@
 #include <SoftwareSerial.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
+#include <pulseSensorPlayground.h>
 
 
 
-
+//body temperature sensor
 #define bodyTempPin 9
 OneWire bTempPin(bodyTempPin);
 DallasTemperature bodyTempSensor(&bTempPin);
 
+//heart pulse sensor
+#define USE_ARDUINO_INTERRUPTS true
+PulseSensorPlayground pulseSensor;
+const int PulseWire = A2;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;  
 
-//create function to read temperature
+
+//function to read temperature
 void readBodyTemp();
 
+//fuction to read pulse
+void readPulse();
 
-
-
-//define serial pins as  2, 3
-// SoftwareSerial mySerial(6, 7); // RX, TX
 
 void setup() {
   Serial1.begin(9600);
   bodyTempSensor.begin();
+
+  //pulse sensor
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);  
+  if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");
+  }
+
 }
 
 
 void loop() { 
   readBodyTemp();
+  readPulse();
 }
 
 
@@ -35,4 +51,14 @@ void readBodyTemp() {
   bodyTempSensor.requestTemperatures();
   Serial1.println(bodyTempSensor.getTempCByIndex(0));
   delay(10);
+}
+
+void readPulse() {
+  int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+  // "myBPM" hold this BPM value now.
+  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+    myBPM = pulseSensor.getBeatsPerMinute();                 
+  }
+  Serial.println(myBPM); 
+  delay(10);                    // considered best practice in a simple sketch.
 }
